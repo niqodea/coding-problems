d086817bca5a2b5ab5c9d257138e1fd4df6680c5
Nicola De Angeli <nicoladeangeli@protonmail.com>
2023-09-22 20:05:58 +0200

    chore: add leetcode template

---
+++ leetcode/template/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def solve(self, a: str, b: int) -> bool:
+        ...
+
+
+if __name__ == "__main__":
+    import csv
+    from pathlib import Path
+
+    solution = Solution()
+
+    test_csv_path = Path(__file__).parent / "test.csv"
+    with open(test_csv_path, mode="r") as test_csv:
+        reader = csv.DictReader(test_csv)
+        for test_row in reader:
+            a = str(test_row["a"])
+            b = int(test_row["b"])
+            expected = {"true": True, "false": False}[test_row["expected"]]
+
+            actual = solution.solve(a, b)
+            assert actual == expected, f"{a=}, {b=}, {expected=}, {actual=}"
+    print("All tests passed!")
